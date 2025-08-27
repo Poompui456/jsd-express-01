8b0abe1a696a356aaecde483b12fd119be8c29c4
Siriporn Worranawin <cheenoi30@gmail.com>
2025-08-27 13:50:14 +0700

    modified:server.js && test-api/members-routes.rest

--- server.js
+++ server.js
@@ -8,26 +8,28 @@ const corsOptions = {
     "http://localhost:5173",
     "http://localhost:5174",
     "http://localhost:5175",
-  ], // frontend domain
+  ],
 };
 
-app.use(cors(corsOptions));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 
-// Middleware to parse JSON bodies
 app.use(express.json());
+// .jason เป็นตัวไปเอา method มาที่ router และทำการแปลงเป็น JS เพื่อให้ระบบอ่านข้อมูลได้ง่าย
 
-app.get("/", (req, res) => {
+// Route 1  : ส่ง get ไปเอาข้อมูล
+app.get("/", (_req, res) => {
   res.send("Hello USER, I am your Server ^u^");
 });
 
-// Declare a simple array called “members” to temporarily store members
 let members = [];
 
-// Route to handle POST requests to create a new post
+// Router 2
 app.post("/members", (req, res) => {
   const { name, lastname, position } = req.body;
-
-  // Example: Save the post to a data store
   const newMember = {
     id: String(members.length + 1),
     name: name,
@@ -36,44 +38,38 @@ app.post("/members", (req, res) => {
   };
 
   members.push(newMember);
-
-  res.status(201).json(newMember);
+  res.status(200).json(newMember);
 });
 
-// Route to handle GET requests to read posts
+// Route 3 get to read members
 app.get("/members", (req, res) => {
   res.status(200).json(members);
 });
 
+// Route 4 DELETE method
 app.delete("/members/:id", (req, res) => {
   const memberId = req.params.id;
-
   const memberIndex = members.findIndex((member) => member.id === memberId);
-
   if (memberIndex !== -1) {
     members.splice(memberIndex, 1);
 
-    res.status(200).send(`Member with ID ${memberId} deleted successfully.`);
+    res.status(200).send(`Member with ID ${memberId} Deleted!`);
   } else {
-    res.status(404).send("Member not found.");
+    res.status(404).send("Member not found");
   }
 });
-
-// Route to handle PUT requests to update a member
+// ROUTE 5 put req to UPDATE a member
 app.put("/members/:id", (req, res) => {
   const memberId = req.params.id;
   const { name, lastname, position } = req.body;
-
   const member = members.find((m) => m.id === memberId);
-
   if (member) {
     if (name !== undefined) member.name = name;
     if (lastname !== undefined) member.lastname = lastname;
     if (position !== undefined) member.position = position;
-
     res.status(200).json(member);
   } else {
-    res.status(404).send("Member not found.");
+    res.status(404).send("Member not found");
   }
 });
 
